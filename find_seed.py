322b53a6cfaf621c249896b5a83d6aa785652def
Lathil <104256014+Lathil1@users.noreply.github.com>
2022-08-06 19:48:42 +0300

    Add files via upload

--- find_seed.py
+++ find_seed.py
@@ -1,63 +1,29 @@
-from urllib.request import urlopen
-import json
-import re
-import random
 import os
 import sys
+import re
 
+seeds = []
 
-threshold = 13 # only seeds of runs under this many minutes
-
-
-def get_time(run):
-    time_str = run["run"]["times"]["primary"]
-    hours_match = re.search(r"\d+(?=H)", time_str)
-    hours = int(hours_match.group(0)) if hours_match is not None else 0
-
-    minutes_match = re.search(r"\d+(?=M)", time_str)
-    minutes = int(minutes_match.group(0)) if minutes_match is not None else 0
-
-    seconds_match = re.search(r"\d+(?=.?\d{0,3}S)", time_str)
-    seconds = int(seconds_match.group(0)) if seconds_match is not None else 0
-
-    millis_match = re.search(r"\d+(?=S)", time_str)
-    millis = int(millis_match.group(0)) if millis_match is not None else 0
-    return [hours, minutes, seconds, millis]
-
-def get_vid(run):
-    video = str(run["run"]["videos"])
-    link = re.findall(r'(https?://\S+)', video)
-    linkfinal = link[-1]
-    unwanted = ['[', ']', '}', "'"]
-    for i in unwanted :
-        linkfinal = linkfinal.replace(i, '')
-    return linkfinal
-
-def get_seed(run):
-    comment = run["run"]["comment"]
-    regex = re.compile(r"(?<=seed:\s)-?\d+", re.I)
-    match = re.search(regex, comment) or re.search(r"\d{4,}", comment)
-    return match.group(0)
-    
-
-
-def main():
+def get_seed():
     os.chdir(sys.path[0])
-    response = urlopen("""
-        https://www.speedrun.com/api/v1/leaderboards/j1npme6p/category/mkeyl926?var-jlzkwqlz=mln68v0q&var-r8rg67rn=21d4zvp1
-    """)
-    data_json = json.loads(response.read())
-    runs = data_json["data"]["runs"]
-    runs_filtered = list(filter(lambda run: get_time(run)[1] < threshold and get_time(run)[0] == 0, runs))
-    rand_run = random.choice(runs_filtered)
-    seed = get_seed(rand_run)
-    vid = get_vid(rand_run)
-    with open("vid.txt", "w") as f:
-        f.writelines(vid)
-    with open("seed.txt", "w") as f:
-        f.writelines(seed)
-    
+    with open("seeds.txt", "r") as f:
+        seed = f.readline()
+        seedreal = re.search(r"(-?\d{4,})", seed)
+        if seedreal:
+            with open("seed.txt", "w") as f:
+                f.writelines(seedreal.group(0))
+            delete_line()
+        else:
+            delete_line()
+            get_seed()
 
-
-if __name__ == "__main__":
-    main()
+def delete_line():
+    os.chdir(sys.path[0])
+    with open ("seeds.txt", "r") as f:
+        seeds = f.readlines()
+    with open("seeds.txt", "w") as f:
+        for i, line in enumerate(seeds):
+            if i not in [0]:
+                f.write(line)
+   
+get_seed()
